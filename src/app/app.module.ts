e0c3fe3c37e707d71ff81e85870455ab2da0e55c
aphang <adriennephang@gmail.com>
2017-09-20 16:15:44 -0500

    adding departments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DepartmentsComponent } from './departments/departments.component';
 
 const appRoutes: Routes =[
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-
+  { path: 'departments', component: DepartmentsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
