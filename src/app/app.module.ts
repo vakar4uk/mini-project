c777a2c893d7beb473a8de97227130a3c53fd1ca
aphang <adriennephang@gmail.com>
2017-09-21 10:27:19 -0500

    Employee html and css
    added employee html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 const appRoutes: Routes =[
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'departments', component: DepartmentsComponent}
+  { path: 'departments', component: DepartmentsComponent},
+  { path: 'employees', component: EmployeesComponent}
 ];
 
 @NgModule({
@@ -20,7 +22,8 @@ const appRoutes: Routes =[
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    DepartmentsComponent
+    DepartmentsComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
