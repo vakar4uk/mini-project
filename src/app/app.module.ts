b6d7a6dc6dc16eaa85adfd9bab7df5ee7ba887ff
aphang <adriennephang@gmail.com>
2017-09-21 14:16:41 -0500

    Revert "ignore"
    This reverts commit 5bd553f758fc78c7cb1986a27bbe70bd5be3dc7d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,31 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+<<<<<<< HEAD
 import { DepartmentsComponent } from './departments/departments.component';
 import { EmployeesComponent } from './employees/employees.component';
+=======
 import { InventoryComponent } from './inventory/inventory.component';
 import { PatientComponent } from './patient/patient.component';
 import { BillingComponent } from './billing/billing.component';
 import { InsuranceComponent } from './insurance/insurance.component';
 import { SchedulingComponent } from './scheduling/scheduling.component';
+>>>>>>> master
 
 const appRoutes: Routes =[
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+<<<<<<< HEAD
   { path: 'departments', component: DepartmentsComponent},
-  { path: 'employees', component: EmployeesComponent},
+  { path: 'employees', component: EmployeesComponent}
+=======
   { path: 'patient', component: PatientComponent },
   { path: 'billing', component: BillingComponent },
   { path: 'insurance', component: InsuranceComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: 'scheduling', component: SchedulingComponent }
 
+>>>>>>> master
 ];
 
 @NgModule({
@@ -33,13 +39,16 @@ const appRoutes: Routes =[
     AppComponent,
     LoginComponent,
     DashboardComponent,
+<<<<<<< HEAD
     DepartmentsComponent,
-    EmployeesComponent,
+    EmployeesComponent
+=======
     InventoryComponent,
     PatientComponent,
     BillingComponent,
     InsuranceComponent,
     SchedulingComponent
+>>>>>>> master
   ],
   imports: [
     BrowserModule,
