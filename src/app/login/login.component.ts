109a7284dfda8c8461eb637336929372876c4b83
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2017-09-22 10:24:37 -0500

    ver. 3.0
    Validation added to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
+  @ViewChild('f') signupForm: NgForm;
 
-  ngOnInit() {
+  onSubmit() {
+    console.log(this.signupForm);
   }
-
 }
