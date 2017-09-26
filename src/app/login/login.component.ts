75b62517b298c2182237c994bc3e7013c3340a4c
charliep4147 <32108295+charliep4147@users.noreply.github.com>
2017-09-26 14:51:46 -0500

    node update 1.0

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,35 @@ import { NgForm } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
-export class LoginComponent {
+// export class InventorysearchComponent implements OnInit {
+  
+//     constructor() { }
+  
+//     ngOnInit() {
+//     }
+  
+//   }
+  
+export class LoginComponent implements OnInit {
   @ViewChild('f') signupForm: NgForm;
 
+  ngOnInit(){
+    var mysql = require('mysql');
+    
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "myusername",
+      password: "mypassword"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  }
   onSubmit() {
     console.log(this.signupForm);
+
+    
   }
 }
