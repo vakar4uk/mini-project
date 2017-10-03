ad6e6c1ea206a6a292eb1ea4776067b5d914bcb4
aphang <adriennephang@gmail.com>
2017-10-03 14:41:20 -0500

    I DON'T KNOW
    Revert revert

--- src/app/scheduling/scheduling.component.ts
+++ src/app/scheduling/scheduling.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-scheduling',
-  templateUrl: './scheduling.component.html',
-  styleUrls: ['./scheduling.component.css']
-})
-export class SchedulingComponent implements OnInit {
+import {ScheduleModule} from 'primeng/primeng';
 
-  constructor() { }
 
-  ngOnInit() {
+export class SchedulingComponent {
+  
+      events: any[];
+      headerConfig: any;
+      ngOnInit() {
+          this.events = [
+              {
+                  "title": "All Day Event",
+                  "start": "2016-01-01"
+              },
+              {
+                  "title": "Long Event",
+                  "start": "2016-01-07",
+                  "end": "2016-01-10"
+              },
+              {
+                  "title": "Repeating Event",
+                  "start": "2016-01-09T16:00:00"
+              },
+              {
+                  "title": "Repeating Event",
+                  "start": "2016-01-16T16:00:00"
+              },
+              {
+                  "title": "Conference",
+                  "start": "2016-01-11",
+                  "end": "2016-01-13"
+              }
+          ];
+      }
+      
   }
-
-}
