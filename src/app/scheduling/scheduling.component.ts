2cb3e4f8b5996fdefe27ff32e246f8f8565dcb3c
aphang <adriennephang@gmail.com>
2017-10-03 14:41:28 -0500

    Revert "I DON'T KNOW"
    This reverts commit ad6e6c1ea206a6a292eb1ea4776067b5d914bcb4.

--- src/app/scheduling/scheduling.component.ts
+++ src/app/scheduling/scheduling.component.ts
@@ -1,36 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
-import {ScheduleModule} from 'primeng/primeng';
+@Component({
+  selector: 'app-scheduling',
+  templateUrl: './scheduling.component.html',
+  styleUrls: ['./scheduling.component.css']
+})
+export class SchedulingComponent implements OnInit {
 
+  constructor() { }
 
-export class SchedulingComponent {
-  
-      events: any[];
-      headerConfig: any;
-      ngOnInit() {
-          this.events = [
-              {
-                  "title": "All Day Event",
-                  "start": "2016-01-01"
-              },
-              {
-                  "title": "Long Event",
-                  "start": "2016-01-07",
-                  "end": "2016-01-10"
-              },
-              {
-                  "title": "Repeating Event",
-                  "start": "2016-01-09T16:00:00"
-              },
-              {
-                  "title": "Repeating Event",
-                  "start": "2016-01-16T16:00:00"
-              },
-              {
-                  "title": "Conference",
-                  "start": "2016-01-11",
-                  "end": "2016-01-13"
-              }
-          ];
-      }
-      
+  ngOnInit() {
   }
+
+}
